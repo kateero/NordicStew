d46f7645a9d76554490105d7a28c2a753c68eaaa
kateero <katyalime785@gmail.com>
2025-03-17 16:04:18 +0300

    Создан контроллер

---
+++ PracticDecorator/src/main/java/mephi/b22901/kateero/practicdecorator/Controller.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mephi.b22901.kateero.practicdecorator;
+
+/**
+ *
+ * @author Катя
+ */
+public class Controller {
+    
+}
